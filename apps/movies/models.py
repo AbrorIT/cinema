3974e4e7280d20bbbd5a91707e151af0256f2f9c
Zinatoy <98818055+Zinatoy@users.noreply.github.com>
2022-05-03 16:38:50 +0600

    models/rating,reviews,ratingstar

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -51,3 +51,45 @@ class MovieComment(models.Model):
     class Meta:
         verbose_name = "Коментарий"
         verbose_name_plural = "Коментарии"
+
+    
+class RatingStar(models.Model): 
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return f'{self.value}'
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+        ordering = ["-value"]
+
+
+class Rating(models.Model): 
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, related_name= "star_rating", verbose_name="звезда")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name = "movie_rating", verbose_name="фильм")
+
+    def __str__(self):
+        return f"{self.star} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+
+class Reviews(models.Model): 
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey(
+        'self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE, related_name="movie_reviews")
+
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
